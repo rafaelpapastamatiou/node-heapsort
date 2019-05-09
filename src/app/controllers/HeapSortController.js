ac641998b85f88318dfbc5100ce3f2e5f60598a6
rafaelpapastamatiou <rafael@teamhub.com.br>
2019-05-09 00:12:36 -0300

    Implementação dos métodos que compõe o HeapSort

--- src/app/controllers/HeapSortController.js
+++ src/app/controllers/HeapSortController.js
@@ -1,6 +1,31 @@
 class HeapSortController {
-  async createMaxHeap() {}
-  async refaz() {}
-  async heapSort() {}
+  async index() {}
+  async heapSort(v) {
+    await constroiMaxHeap(v);
+    let n = v.Length;
+    for (let i = v.Length - 1; i > 0; i--) {
+      await troca(v, i, 0);
+      await refaz(v, 0, --n);
+    }
+  }
+  async constroiMaxHeap(v) {
+    for (let i = v.Length / 2 - 1; i >= 0; i--) refaz(v, i, v.Length);
+  }
+  async refaz(vetor, pos, tamanhoDoVetor) {
+    let max = 2 * pos + 1,
+      right = max + 1;
+    if (max < tamanhoDoVetor) {
+      if (right < tamanhoDoVetor && vetor[max] < vetor[right]) max = right;
+      if (vetor[max] > vetor[pos]) {
+        await troca(vetor, max, pos);
+        await refaz(vetor, max, tamanhoDoVetor);
+      }
+    }
+  }
+  async troca(v, j, aposJ) {
+    let aux = v[j];
+    v[j] = v[aposJ];
+    v[aposJ] = aux;
+  }
 }
 module.exports = HeapSortController;
--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 const express = require("express");
 const routes = express.Router();
-
+const HeapSortController = require("./app/controllers/HeapSortController");
+routes.use("/heapsort", HeapSortController.index);
 module.exports = routes;
